f46b9402f9f313e1e39acb87c3bc63ba5f78a4d7
Chaan <rnchagwe27@gmail.com>
2024-05-25 22:18:43 +0800

    added styles

---
+++ main.js
@@ -0,0 +1,16 @@
+const ops = [
+    "Tell me about yourself.",
+    "What is website development?",
+    "What is Front-end development?",
+    "What are/is your strength/s and weakness/es?",
+    "Elaborate what is framework.",
+    "Explain the HTTP & HTTP/2.",
+    "What's your most struggle from your past experience?",
+    "What is web3 and how do you adhere to it in your web design?",
+    "What do you know about Cpanel?",
+    "What is Wordpress?",
+    "What do you know about boostrap?",
+]
+
+const used = new Set()
+
